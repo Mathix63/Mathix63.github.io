a4723da736a998a6721242c6cc05183495e7343e
Mathix63 <matheo.sepulcre@gmail.com>
2021-12-02 17:34:16 +0100

    ok

--- 2/script.js
+++ 2/script.js
@@ -1,1 +1,12 @@
-console.log("salut")
+
+const button = document.getElementById("bvalid");
+const text = document.getElementById("rep");
+
+button.onclick = function(e){
+	if(text.value==="9" || text.value==="9") {
+		alert("Allumes le micro-ondes pendant 10 sec");
+	} else {
+		alert("HAHAHAHAHHA NON");
+	}
+}
+
