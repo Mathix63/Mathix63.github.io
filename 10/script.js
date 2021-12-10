6516d3e098f34dd08a8d4cb6b53e141205365b80
Mathix63 <matheo.sepulcre@gmail.com>
2021-12-10 18:40:50 +0100

    yo

---
+++ 10/script.js
@@ -0,0 +1,12 @@
+
+const button = document.getElementById("bvalid");
+const text = document.getElementById("rep");
+
+button.onclick = function(e){
+	if(text.value==="66955" || text.value==="6955") {
+		alert("Viens me dire que je suis super beau et t'auras des chocos");
+	} else {
+		alert("RETROMPES TOI ENCORE UNE FOIS ET JE TE SPOIL ONE PIECE");
+	}
+}
+
